cf4ba87bbe4ebcc8d8a097fe8888b26516eb55d1
qiyueshiyi <qiyueshiyi@outlook.com>
2017-03-07 15:52:56 +0800

    Add notify function to Notifier class

--- src/Notifier.php
+++ src/Notifier.php
@@ -10,23 +10,12 @@
 namespace Notadd\Member;
 
 use Notadd\Member\Models\Member;
+use Notadd\Member\Models\Notification;
 
 class Notifier
 {
     public function notify($type, Member $sender, Member $toUser, $subject = null, $subjectType = null, $reply = null)
     {
-        if ($sender->id == $toUser->id) {
-            return;
-        }
-
-        return Member::create([
-            'sender_id'    => $sender->id,
-            'user_id'      => $toUser->id,
-            'subject_id'   => $subject ? $subject->id : 0,
-            'subject_type' => $subjectType,
-            'reply_id'     => $reply ? $reply->id : 0,
-            'body'         => $reply ? $reply->body : '',
-            'type'         => $type,
-        ]);
+        return Notification::notify($type, $sender, $toUser, $subject, $subjectType, $reply);
     }
 }
