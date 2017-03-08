beed376611a3c5cbde72025a38c4f51588f4a148
twilroad <269044570@qq.com>
2017-03-08 15:07:58 +0800

    add routes and components

--- resources/mixes/administration/src/components/Layout.vue
+++ resources/mixes/administration/src/components/Layout.vue
@@ -12,79 +12,20 @@
       return {
         nav: [
           {
-            text: '文章管理',
-            icon: 'fa fa-file-text-o',
-            uri: '#',
-            menu: [
-              {
-                text: '全部文章',
-                uri: '/content/article/all'
-              },
-              {
-                text: '分类管理',
-                uri: '/content/article/category'
-              },
-              {
-                text: '回收站',
-                icon: 'fa fa-circle-o',
-                uri: '/content/article/recycle'
-              },
-              {
-                text: '草稿箱',
-                icon: 'fa fa-circle-o',
-                uri: '/content/article/draft'
-              }/* ,
-                 {
-                 text: '标签管理',
-                 icon: 'fa fa-circle-o',
-                 uri: '/content/article/tag'
-                 }, */
-            ]
-          },
-          {
-            text: '页面管理',
+            text: '用户管理',
             icon: 'fa fa-file-o',
-            uri: '#',
-            menu: [
-              {
-                text: '全部页面',
-                icon: 'fa fa-circle-o',
-                uri: '/content/page/all'
-              },
-              {
-                text: '分类管理',
-                icon: 'fa fa-circle-o',
-                uri: '/content/page/category'
-              }
-            ]
+            uri: '/member/user'
           },
           {
-            text: '模块组件',
-            icon: 'fa fa-building',
-            uri: '#',
-            menu: [
-              {
-                text: 'SEO配置',
-                icon: 'fa fa-circle-o',
-                uri: '/content/component'
-              }
-            ]
-          }/* ,
-             {
-             text: '模板管理',
-             icon: 'fa fa-circle-o',
-             uri: '/content/template'
-             },
-             {
-             text: '插件管理',
-             icon: 'fa fa-circle-o',
-             uri: '/content/extension'
-             },
-             {
-             text: '评论功能',
-             icon: 'fa fa-circle-o',
-             uri: '/content/comment'
-             } */
+            text: '用户组管理',
+            icon: 'fa fa-file-o',
+            uri: '/member/group'
+          },
+          {
+            text: '安全管理',
+            icon: 'fa fa-file-o',
+            uri: '/member/security'
+          }
         ]
       }
     }
