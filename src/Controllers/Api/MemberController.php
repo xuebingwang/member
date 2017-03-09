f14246466c181b9308eb192daddac9f048d854c2
qiyueshiyi <qiyueshiyi@outlook.com>
2017-03-09 18:11:39 +0800

    修改更新用户数据 api bug, 修改返回的用户数据, 添加 /api/member/members/{member_id}/update api and update function to api MemberController

--- src/Controllers/Api/MemberController.php
+++ src/Controllers/Api/MemberController.php
@@ -9,7 +9,9 @@
 
 namespace Notadd\Member\Controllers\Api;
 
+use Notadd\Member\Models\Group;
 use Notadd\Member\Models\Member;
+use Notadd\Member\Models\Permission;
 use Notadd\Member\Abstracts\AbstractApiController;
 
 class MemberController extends AbstractApiController
@@ -24,14 +26,20 @@ class MemberController extends AbstractApiController
 
         return $this->respondWithPaginator($lists, function (Member $list) {
             return [
-                'id'         => $list->id,
-                'nick_name'  => $list->nick_name,
-                'sex'        => sex_trans($list->sex),
-                'age'        => $list->birth_date ? $list->birth_date->age : '',
-                'avatar'     => $list->avatar,
-                'points'     => $list->points,
-                'group'      => $list->cachedGroups()->implode('display_name', '|'),
-                'created_at' => $list->created_at->toDateTimeString(),
+                'id'           => $list->id,
+                'name'         => $list->name,
+                'email'        => $list->email,
+                'phone'        => $list->phone,
+                'nick_name'    => $list->nick_name,
+                'real_name'    => $list->real_name,
+                'sex'          => sex_trans($list->sex),
+                'age'          => $list->birth_date ? $list->birth_date->age : '',
+                'avatar'       => $list->avatar,
+                'points'       => $list->points,
+                'signature'    => $list->signature,
+                'introduction' => $list->introduction,
+                'group'        => $list->cachedGroups()->implode('display_name', '|'),
+                'created_at'   => $list->created_at->toDateTimeString(),
             ];
         });
     }
@@ -46,15 +54,71 @@ class MemberController extends AbstractApiController
 
         return $this->respondWithItem($member, function (Member $list) {
             return [
-                'id'         => $list->id,
-                'nick_name'  => $list->nick_name,
-                'sex'        => sex_trans($list->sex),
-                'age'        => $list->birth_date ? $list->birth_date->age : '',
-                'avatar'     => $list->avatar,
-                'points'     => $list->points,
-                'group'      => $list->cachedGroups()->implode('display_name', '|'),
-                'created_at' => $list->created_at->toDateTimeString(),
+                'id'           => $list->id,
+                'name'         => $list->name,
+                'email'        => $list->email,
+                'phone'        => $list->phone,
+                'nick_name'    => $list->nick_name,
+                'real_name'    => $list->real_name,
+                'sex'          => sex_trans($list->sex),
+                'age'          => $list->birth_date ? $list->birth_date->age : '',
+                'avatar'       => $list->avatar,
+                'points'       => $list->points,
+                'signature'    => $list->signature,
+                'introduction' => $list->introduction,
+                'group'        => $list->cachedGroups()->implode('display_name', '|'),
+                'created_at'   => $list->created_at->toDateTimeString(),
             ];
         });
+    }
+
+    public function update($member_id)
+    {
+        $member = Member::find(intval($member_id));
+
+        if (! $member || ! $member->exists) {
+            return $this->errorNotFound();
+        }
+
+        $validator = $this->getValidationFactory()->make(
+            $this->request->all(),
+            [
+                'name'       => 'required|unique:members,name,' . $member->id,
+                'email'      => 'required|unique:members,email,' . $member->id,
+                'phone'      => 'required|unique:members,phone,' . $member->id,
+                'birth_date' => 'date',
+            ],
+            [
+                'name.required'   => '请输入用户名.',
+                'name.unique'     => '用户名已经存在.',
+                'email.required'  => '请输入邮箱.',
+                'email.unique'    => '邮箱已经存在.',
+                'phone.required'  => '请输入手机号.',
+                'phone.unique'    => '手机号已经存在.',
+                'birth_date.date' => '无效的出生日期.',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return $this->errorValidate($validator->getMessageBag()->toArray());
+        }
+        $member->fill(array_only($this->request->all(), [
+            'name', 'email', 'phone', 'birth_date', 'sex', 'signature', 'introduction', 'nick_name', 'real_name',
+        ]));
+        $member->save();
+
+        $groups = Group::whereIn('id', $this->request->input('groups', []))
+            ->get()
+            ->pluck('id')
+            ->toArray();
+
+        $permissions = Permission::whereIn('id', $this->request->input('permissions', []))
+            ->get()
+            ->pluck('id')
+            ->toArray();
+
+        // 给用户添加用户组
+        $member->groups()->sync($groups);
+        $member->permissions()->sync($permissions);
     }
 }
