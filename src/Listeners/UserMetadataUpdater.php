434ac71ff33f9924d73285250e14f8d0ad44cf22
qiyueshiyi <qiyueshiyi@outlook.com>
2017-03-14 17:14:18 +0800

    Removed unused events and AbstractAdminController

--- src/Listeners/UserMetadataUpdater.php
+++ src/Listeners/UserMetadataUpdater.php
@@ -8,9 +8,7 @@
  */
 namespace Notadd\Member\Listeners;
 
-use Notadd\Member\Events\CheckIn;
 use Notadd\Member\Models\Registration;
-use Notadd\Member\Events\PublishedTopic;
 use Notadd\Member\Models\GetPointsRecord;
 use Illuminate\Contracts\Events\Dispatcher;
 
@@ -21,53 +19,5 @@ class UserMetadataUpdater
 {
     public function subscribe(Dispatcher $events)
     {
-        $events->listen(PublishedTopic::class, [$this, 'publishedTopic']);
-        $events->listen(CheckIn::class, [$this, 'checkIn']);
-    }
-
-    public function publishedTopic(PublishedTopic $event)
-    {
-        $topicPoints = app('points')->get('public-topic');
-        $user = $event->user;
-        if ($topicPoints && $topicPoints->exists && $user) {
-            $user->points += $topicPoints->points;
-            $user->save();
-            GetPointsRecord::create([
-                'user_id'             => $user->id,
-                'action_display_name' => $topicPoints->display_name,
-                'action_name'         => $topicPoints->name,
-                'points'              => $topicPoints->points,
-            ]);
-        }
-    }
-
-    public function checkIn(CheckIn $event)
-    {
-        $user = $event->user;
-        if (!$user) {
-            return;
-        }
-        if ($user->todaySigned()) {
-            return;
-        }
-        $signAction = app('points')->get('sign');
-        if (!$signAction) {
-            return;
-        }
-        Registration::checkIn($user->id, $signAction->points);
-        $user->points += $signAction->points;
-        $user->total_registration_count += 1;
-        if ($user->yesterdaySigned()) {
-            $user->continue_registration_count += 1;
-        } else {
-            $user->continue_registration_count = 1;
-        }
-        $user->save();
-        GetPointsRecord::create([
-            'user_id'             => $user->id,
-            'action_display_name' => $signAction->display_name,
-            'action_name'         => $signAction->name,
-            'points'              => $signAction->points,
-        ]);
     }
 }
