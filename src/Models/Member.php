cfab71c206c04bd0e66b21315405a87f654cd582
qiyueshiyi <qiyueshiyi@outlook.com>
2017-03-31 18:07:01 +0800

    覆写 Member::getRelationValue 方法，以让 Member 支持 $member->address 这样调用注入的数据关联关系

--- src/Models/Member.php
+++ src/Models/Member.php
@@ -369,4 +369,27 @@ class Member extends BaseMember
             $this->detachGroup($group);
         }
     }
+
+    /**
+     * Get a relationship.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function getRelationValue($key)
+    {
+        // If the key already exists in the relationships array, it just means the
+        // relationship has already been loaded, so we'll just return it out of
+        // here because there is no need to query within the relations twice.
+        if ($this->relationLoaded($key)) {
+            return $this->relations[$key];
+        }
+
+        // If the "attribute" exists as a method on the model, we will just assume
+        // it is a relationship and will load and return results from the query
+        // and hydrate the relationship's value on the "relationships" array.
+        if (method_exists($this, $key) || $this->hasInjectedFunction($key)) {
+            return $this->getRelationshipFromMethod($key);
+        }
+    }
 }
