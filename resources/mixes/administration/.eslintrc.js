c9763cbf0e52233d8078e1fdfc87c5e046424e8f
twilroad <269044570@qq.com>
2017-04-16 21:10:24 +0800

    add not exists packages

--- resources/mixes/administration/.eslintrc.js
+++ resources/mixes/administration/.eslintrc.js
@@ -9,12 +9,19 @@ module.exports = {
     env: {
         browser: true,
     },
-    // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
     extends: 'airbnb-base',
     // required to lint *.vue files
     plugins: [
         'html'
     ],
+    // check if imports actually resolve
+    'settings': {
+        'import/resolver': {
+            'webpack': {
+                'config': 'build/webpack.base.conf.js'
+            }
+        }
+    },
     // add your custom rules here
     'rules': {
         // don't require .vue extension when importing
