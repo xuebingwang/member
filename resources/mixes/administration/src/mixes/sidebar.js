f192bf2e2538dd499db50385ab81d9b45bfd4718
twilroad <269044570@qq.com>
2017-04-17 15:20:46 +0800

    add sidebar menus with active codes

---
+++ resources/mixes/administration/src/mixes/sidebar.js
@@ -0,0 +1,19 @@
+export default function (injection) {
+    injection.useSidebar('member', [
+        {
+            icon: 'plus',
+            path: '/member/user',
+            title: '用户管理',
+        },
+        {
+            icon: 'plus',
+            path: '/member/group',
+            title: '用户组管理',
+        },
+        {
+            icon: 'plus',
+            path: '/member/security',
+            title: '安全管理',
+        },
+    ]);
+}
--- resources/mixes/administration/src/pages/Dashboard.vue
+++ resources/mixes/administration/src/pages/Dashboard.vue
@@ -1,5 +1,11 @@
 <script>
+    import injection from '../helpers/injection';
+
     export default {
+        beforeRouteEnter(to, from, next) {
+            injection.sidebar.active('member');
+            next();
+        },
     }
 </script>
 <template></template>
