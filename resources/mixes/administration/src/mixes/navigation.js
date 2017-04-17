f8d885084843f2a6ed85f43701ba88db3be834d6
twilroad <269044570@qq.com>
2017-04-17 14:29:14 +0800

    add navigation item

--- resources/mixes/administration/src/mixes/navigation.js
+++ resources/mixes/administration/src/mixes/navigation.js
@@ -1,1 +1,7 @@
-export default {};
+export default function (injection) {
+    injection.useNavigation({
+        icon: 'person-stalker',
+        path: '/member',
+        title: '用户中心',
+    });
+}
