680a46df5f17c49b4135053b27efe80340043e8a
qiyueshiyi <qiyueshiyi@outlook.com>
2017-04-18 17:39:48 +0800

    Add generateToken function to EmailVerification

--- src/EmailVerification.php
+++ src/EmailVerification.php
@@ -9,6 +9,12 @@
 
 namespace Notadd\Member;
 
+use Illuminate\Support\Str;
+
 class EmailVerification
 {
+    protected function generateToken()
+    {
+        return hash_hmac('sha256', Str::random(40), config('app.key'));
+    }
 }
