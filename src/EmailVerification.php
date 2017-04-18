ba86405d035af1d2393383594fe2a05714589def
qiyueshiyi <qiyueshiyi@outlook.com>
2017-04-18 17:31:06 +0800

    Add EmailVerification class

---
+++ src/EmailVerification.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author        Qiyueshiyi <qiyueshiyi@outlook.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime      2017-04-18 17:27
+ */
+
+namespace Notadd\Member;
+
+class EmailVerification
+{
+}
