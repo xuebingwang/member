641aa0a5251c0d2bd78bdeed950724f1e7e5a920
twilroad <269044570@qq.com>
2017-04-18 17:05:58 +0800

    add avatar upload logic

--- src/Handlers/Upload/UploadHandler.php
+++ src/Handlers/Upload/UploadHandler.php
@@ -10,6 +10,8 @@ namespace Notadd\Member\Handlers\Upload;
 
 use Illuminate\Container\Container;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Notadd\Foundation\Passport\Abstracts\SetHandler;
 
 /**
@@ -42,6 +44,50 @@ class UploadHandler extends SetHandler
      */
     public function execute()
     {
+        $this->validate($this->request, [
+            'file' => 'required|image',
+        ], [
+            'file.image'    => '上传文件格式必须为图片格式！',
+            'file.required' => '必须上传一个文件！',
+        ]);
+        $avatar = $this->request->file('file');
+        $hash = hash_file('md5', $avatar->getPathname(), false);
+        $dictionary = $this->pathSplit($hash, '12', Collection::make([
+            'uploads',
+        ]))->implode(DIRECTORY_SEPARATOR);
+        if (!$this->files->isDirectory(app_path($dictionary))) {
+            $this->files->makeDirectory(app_path($dictionary), 0777, true, true);
+        }
+        $file = Str::substr($hash, 12, 20) . '.' . $avatar->getClientOriginalExtension();
+        if (!$this->files->exists($dictionary . DIRECTORY_SEPARATOR . $file)) {
+            $avatar->move($dictionary, $file);
+        }
+        $this->data['path'] = $this->pathSplit($hash, '12,20', Collection::make([
+                'uploads',
+            ]))->implode('/') . '.' . $avatar->getClientOriginalExtension();
+
         return true;
+    }
+
+    /**
+     * String split handler.
+     *
+     * @param string $path
+     * @param string $dots
+     * @param null   $data
+     *
+     * @return \Illuminate\Support\Collection|null
+     */
+    protected function pathSplit($path, $dots, $data = null)
+    {
+        $dots = explode(',', $dots);
+        $data = $data ? $data : new Collection();
+        $offset = 0;
+        foreach ($dots as $dot) {
+            $data->push(Str::substr($path, $offset, $dot));
+            $offset += $dot;
+        }
+
+        return $data;
     }
 }
