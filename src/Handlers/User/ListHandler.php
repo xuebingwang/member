2ba332590ea426e8178e1245ebfb3a9986278a31
twilroad <269044570@qq.com>
2017-04-21 15:52:04 +0800

    return raw data from handler

--- src/Handlers/User/ListHandler.php
+++ src/Handlers/User/ListHandler.php
@@ -8,11 +8,32 @@
  */
 namespace Notadd\Member\Handlers\User;
 
+use Illuminate\Container\Container;
 use Notadd\Foundation\Passport\Abstracts\DataHandler;
+use Notadd\Member\Models\Member;
 
 /**
  * Class ListHandler.
  */
 class ListHandler extends DataHandler
 {
+    /**
+     * ListHandler constructor.
+     *
+     * @param \Illuminate\Container\Container $container
+     * @param \Notadd\Member\Models\Member    $member
+     */
+    public function __construct(Container $container, Member $member)
+    {
+        parent::__construct($container);
+        $this->model = $member;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function data()
+    {
+        return $this->model->newQuery()->orderBy('created', 'desc')->paginate(20);
+    }
 }
