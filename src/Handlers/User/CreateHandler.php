a12075e9eb9479af3edbfed0d05ff234f07944fe
twilroad <269044570@qq.com>
2017-04-21 17:20:26 +0800

    add logic for user/create api

--- src/Handlers/User/CreateHandler.php
+++ src/Handlers/User/CreateHandler.php
@@ -8,11 +8,32 @@
  */
 namespace Notadd\Member\Handlers\User;
 
+use Illuminate\Container\Container;
 use Notadd\Foundation\Passport\Abstracts\SetHandler;
+use Notadd\Member\Models\Member;
 
 /**
  * Class Create.
  */
 class CreateHandler extends SetHandler
 {
+    /**
+     * Create constructor.
+     *
+     * @param \Illuminate\Container\Container $container
+     * @param \Notadd\Member\Models\Member    $member
+     */
+    public function __construct(Container $container, Member $member)
+    {
+        parent::__construct($container);
+        $this->model = $member;
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function execute()
+    {
+        return $this->model->newQuery()->create($this->request->all());
+    }
 }
