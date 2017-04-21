13fa3a4b57a26e40a7cda5de9203f15e82f1a9d6
twilroad <269044570@qq.com>
2017-04-21 17:48:48 +0800

    add logic for group/list api

--- src/Handlers/Group/ListHandler.php
+++ src/Handlers/Group/ListHandler.php
@@ -17,4 +17,88 @@ use Notadd\Member\Models\Group;
  */
 class ListHandler extends DataHandler
 {
+    /**
+     * @var string
+     */
+    protected $format;
+
+    /**
+     * @var string
+     */
+    protected $order;
+
+    /**
+     * @var int
+     */
+    protected $paginate;
+
+    /**
+     * @var \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    protected $pagination;
+
+    /**
+     * @var string
+     */
+    protected $sort;
+
+    /**
+     * ListHandler constructor.
+     *
+     * @param \Illuminate\Container\Container $container
+     * @param \Notadd\Member\Models\Group     $group
+     */
+    public function __construct(Container $container, Group $group)
+    {
+        parent::__construct($container);
+        $this->format = 'raw';
+        $this->model = $group;
+        $this->order = 'created_at';
+        $this->paginate = 20;
+        $this->sort = 'desc';
+    }
+
+    public function configurations()
+    {
+        $this->format = $this->request->input('format') ?: $this->format;
+        $this->order = $this->request->input('order') ?: $this->order;
+        $this->paginate = $this->request->input('paginate') ?: $this->paginate;
+        $this->sort = $this->request->input('sort') ?: $this->sort;
+    }
+
+    /**
+     * @return array
+     */
+    public function data()
+    {
+        $this->configurations();
+
+        $this->pagination = $this->model->newQuery()->orderBy($this->order, $this->sort)->paginate($this->paginate);
+
+        return $this->pagination->items();
+    }
+
+    /**
+     * Make data to response with errors or messages.
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse
+     * @throws \Exception
+     */
+    public function toResponse()
+    {
+        $response = parent::toResponse();
+
+        return $response->withParams([
+            'pagination' => [
+                'count'    => $this->pagination->total(),
+                'current'  => $this->pagination->currentPage(),
+                'from'     => $this->pagination->firstItem(),
+                'next'     => $this->pagination->nextPageUrl(),
+                'paginate' => $this->paginate,
+                'prev'     => $this->pagination->previousPageUrl(),
+                'to'       => $this->pagination->lastItem(),
+                'total'    => $this->pagination->lastPage(),
+            ],
+        ]);
+    }
 }
