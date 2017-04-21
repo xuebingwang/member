0c4d569f08aacafd3030e5a5e94ccc5f955e0b0a
twilroad <269044570@qq.com>
2017-04-21 17:06:53 +0800

    add logic for user api

--- src/Handlers/User/UserHandler.php
+++ src/Handlers/User/UserHandler.php
@@ -8,11 +8,52 @@
  */
 namespace Notadd\Member\Handlers\User;
 
+use Illuminate\Container\Container;
 use Notadd\Foundation\Passport\Abstracts\DataHandler;
+use Notadd\Member\Models\Member;
 
 /**
  * Class UserHandler.
  */
 class UserHandler extends DataHandler
 {
+    /**
+     * @var string
+     */
+    protected $format;
+
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * UserHandler constructor.
+     *
+     * @param \Illuminate\Container\Container $container
+     * @param \Notadd\Member\Models\Member    $member
+     */
+    public function __construct(Container $container, Member $member)
+    {
+        parent::__construct($container);
+        $this->format = 'raw';
+        $this->id = 1;
+        $this->model = $member;
+    }
+
+    public function configurations()
+    {
+        $this->format = $this->request->input('format') ?: $this->format;
+        $this->id = $this->request->input('id') ?: $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function data()
+    {
+        $this->configurations();
+
+        return $this->model->newQuery()->find($this->id);
+    }
 }
