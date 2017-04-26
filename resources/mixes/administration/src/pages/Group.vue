2a367bb2da0305c7bbca900d5a62ee0108fed68f
twilroad <269044570@qq.com>
2017-04-26 16:58:57 +0800

    using /member/group/list instead of /member/groups/index

--- resources/mixes/administration/src/pages/Group.vue
+++ resources/mixes/administration/src/pages/Group.vue
@@ -4,7 +4,7 @@
     export default {
         beforeRouteEnter(to, from, next) {
             injection.loading.start();
-            injection.http.post(`${window.api}/member/groups/index`).then(response => {
+            injection.http.post(`${window.api}/member/group/list`).then(response => {
                 next(vm => {
                     vm.list = response.data.data;
                     injection.loading.finish();
