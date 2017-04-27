95a3288443ed6a6024b430242d5df45e3cf0611a
twilroad <269044570@qq.com>
2017-04-27 15:33:21 +0800

    add logic for user ban page

--- resources/mixes/administration/src/pages/UserBan.vue
+++ resources/mixes/administration/src/pages/UserBan.vue
@@ -4,11 +4,23 @@
     export default {
         beforeRouteEnter(to, from, next) {
             injection.loading.start();
-            injection.http.post(`${window.api}/member/members/${to.params.id}/show`).then(response => {
+            injection.http.post(`${window.api}/member/user`, {
+                id: to.params.id,
+                with: [
+                    'ban',
+                ],
+            }).then(response => {
                 const data = response.data.data;
                 next(vm => {
                     vm.form.id = data.id;
                     vm.form.name = data.name;
+                    if (data.ban) {
+                        vm.form.end = data.ban.end;
+                        vm.form.now = data.ban.type;
+                        vm.form.reason = data.ban.reason;
+                        vm.form.time = data.ban.time;
+                        vm.form.type = data.ban.type;
+                    }
                     injection.loading.finish();
                     injection.sidebar.active('member');
                 });
@@ -19,31 +31,64 @@
         data() {
             return {
                 form: {
+                    end: '',
                     id: '',
                     name: '',
+                    now: 0,
                     reason: '',
-                    status: '',
-                    type: '',
+                    time: 0,
+                    type: 0,
                 },
-                rules: {
-                    name: [
-                        {
-                            required: true,
-                            type: 'string',
-                            message: '请输入用户组名称',
-                            trigger: 'change',
-                        },
-                    ],
-                },
+                loading: false,
+                rules: {},
+                times: [
+                    {
+                        label: '永久',
+                        value: 0,
+                    },
+                    {
+                        label: '一天',
+                        value: 1,
+                    },
+                    {
+                        label: '一周',
+                        value: 2,
+                    },
+                    {
+                        label: '一个月',
+                        value: 3,
+                    },
+                    {
+                        label: '一年',
+                        value: 4,
+                    },
+                    {
+                        label: '其他时间',
+                        value: 5,
+                    },
+                ],
             };
         },
         methods: {
+            dateChange(date) {
+                this.form.end = date;
+            },
             submit() {
                 const self = this;
                 self.loading = true;
                 self.$refs.form.validate(valid => {
                     if (valid) {
-                        window.console.log(valid);
+                        self.$loading.start();
+                        self.$http.post(`${window.api}/member/user/ban`, self.form).then(response => {
+                            self.$loading.finish();
+                            self.$notice.open({
+                                title: response.data.message,
+                            });
+                            self.$router.push('/member/user');
+                        }).catch(() => {
+                            self.$loading.error();
+                            self.loading = false;
+                        });
                     } else {
                         self.loading = false;
                         self.$notice.error({
@@ -66,21 +111,53 @@
                 <i-form :label-width="200" :model="form" ref="form" :rules="rules">
                     <row>
                         <i-col span="14">
-                            <form-item label="禁止用户名" prop="name">
-                                <i-input placeholder="请输入禁止用户名" v-model="form.name"></i-input>
+                            <form-item label="禁止用户名">
+                                <span>{{ form.name }}</span>
                             </form-item>
                         </i-col>
                     </row>
                     <row>
                         <i-col span="14">
-                            <form-item label="当前状态" prop="name">
-                                <span>正常状态</span>
+                            <form-item label="当前状态">
+                                <span v-if="form.now === 0">不封禁</span>
+                                <span v-if="form.now > 0 && form.now < 3">部分封禁</span>
+                                <span v-if="form.now > 2">完全封禁</span>
                             </form-item>
                         </i-col>
                     </row>
                     <row>
                         <i-col span="14">
-                            <form-item label="禁止类型" prop="name">
+                            <form-item label="禁止类型">
+                                <radio-group v-model="form.type" size="large" vertical>
+                                    <radio label="0">
+                                        <span>不封禁</span>
+                                    </radio>
+                                    <radio label="1">
+                                        <span>可浏览网站开放内容</span>
+                                    </radio>
+                                    <radio label="2">
+                                        <span>可浏览个人资料</span>
+                                    </radio>
+                                    <radio label="3">
+                                        <span>禁止登陆</span>
+                                    </radio>
+                                </radio-group>
+                            </form-item>
+                        </i-col>
+                    </row>
+                    <row>
+                        <i-col span="8">
+                            <form-item label="封禁时间">
+                                <i-select v-model="form.time">
+                                    <i-option v-for="item in times" :value="item.value" :key="item">{{ item.label }}</i-option>
+                                </i-select>
+                                <date-picker :placeholder="请选择封禁截止时间"
+                                             style="margin-top: 20px;"
+                                             type="date"
+                                             :value="form.end"
+                                             v-if="form.time === 5"
+                                             @on-change="dateChange">
+                                </date-picker>
                             </form-item>
                         </i-col>
                     </row>
