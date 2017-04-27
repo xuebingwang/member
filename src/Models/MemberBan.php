b6b07a694ccd0bf847bbb8eb9ddf8b1a6794a9a1
twilroad <269044570@qq.com>
2017-04-27 14:47:52 +0800

    add model for member_banned table

---
+++ src/Models/MemberBan.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-04-27 14:41
+ */
+namespace Notadd\Member\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class MemberBan.
+ */
+class MemberBan extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'reason',
+        'type',
+        'time',
+        'user_id',
+    ];
+
+    /**
+     * @var string
+     */
+    protected $table = 'member_banned';
+}
