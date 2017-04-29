9a377cd528dadf87e9bebeb876ce21009aa4deb5
twilroad <269044570@qq.com>
2017-04-29 14:54:16 +0800

    create migration file for member_activates table

---
+++ databases/migrations/2017_04_29_145258_create_member_activates_table.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @datetime 2017-04-29 14:52:58
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Notadd\Foundation\Database\Migrations\Migration;
+
+/**
+ * Class CreateMemberActivatesTable.
+ */
+class CreateMemberActivatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->create('member_activates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->drop('member_activates');
+    }
+}
