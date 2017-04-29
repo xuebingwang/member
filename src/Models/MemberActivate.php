5e269cc7570c169c053f9de719e32e79257fcbaf
twilroad <269044570@qq.com>
2017-04-29 14:59:03 +0800

    add model for member_activates table

---
+++ src/Models/MemberActivate.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-04-29 14:57
+ */
+namespace Notadd\Member\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class MemberActivate.
+ */
+class MemberActivate extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'activated',
+        'context',
+        'member_id',
+        'type',
+    ];
+
+    /**
+     * @var string
+     */
+    protected $table = 'member_activates';
+}
