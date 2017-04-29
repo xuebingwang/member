5fda87f5d023e34108ba34671dac844d8ce1a680
twilroad <269044570@qq.com>
2017-04-29 15:37:23 +0800

    remove eol-last rule

--- resources/mixes/administration/.eslintrc.js
+++ resources/mixes/administration/.eslintrc.js
@@ -1,5 +1,3 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
     root: true,
     parser: 'babel-eslint',
@@ -10,11 +8,9 @@ module.exports = {
         browser: true,
     },
     extends: 'airbnb-base',
-    // required to lint *.vue files
     plugins: [
         'html'
     ],
-    // check if imports actually resolve
     'settings': {
         'import/resolver': {
             'webpack': {
@@ -22,32 +18,27 @@ module.exports = {
             }
         }
     },
-    // add your custom rules here
     'rules': {
-        // don't require .vue extension when importing
         'arrow-parens': [2, "as-needed", {
             "requireForBlockBody": false
         }],
+        'eol-last': 0,
         'guard-for-in': 0,
         'indent': ['error', 4, {
             'SwitchCase': 1
         }],
-        // don't require .vue extension when importing
         'import/extensions': ['error', 'always', {
             'js': 'never',
             'vue': 'never'
         }],
-        // allow optionalDependencies
         'import/no-extraneous-dependencies': ['error', {
             'optionalDependencies': ['test/unit/index.js']
         }],
-        // allow console during development
         'no-console': process.env.NODE_ENV === 'production' ? 0 : 0,
-        // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
         'no-param-reassign': ['error', {
             'props': false
         }],
         'no-restricted-syntax': ['error', 'WithStatement', "BinaryExpression[operator='in']"]
     }
-}
+};
