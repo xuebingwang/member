362fd968195ff6ecffa36973a0ebf247f438aab5
twilroad <269044570@qq.com>
2017-05-04 18:50:00 +0800

    create model for member_information_relations table

---
+++ src/Models/MemberInformationRelation.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-05-04 18:48
+ */
+namespace Notadd\Member\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class MemberInformationRelation.
+ */
+class MemberInformationRelation extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'group_id',
+        'information_id',
+    ];
+
+    /**
+     * @var string
+     */
+    protected $table = 'member_information_relations';
+}
