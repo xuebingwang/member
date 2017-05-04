c5dab81949de6a8e6167051eda34fe31dd98ba40
twilroad <269044570@qq.com>
2017-05-04 20:24:39 +0800

    remove unnecessary codes

--- src/Controllers/Api/PermissionController.php
+++ src/Controllers/Api/PermissionController.php
@@ -2,120 +2,18 @@
 /**
  * This file is part of Notadd.
  *
- * @author Qiyueshiyi <qiyueshiyi@outlook.com>
+ * @author TwilRoad <269044570@qq.com>
  * @copyright (c) 2017, iBenchu.org
- * @datetime 2017-03-09 10:45
+ * @datetime 2017-05-04 20:22
  */
 namespace Notadd\Member\Controllers\Api;
 
-use Notadd\Member\Models\Permission;
-use Notadd\Member\Abstracts\AbstractApiController;
+use Notadd\Foundation\Routing\Abstracts\Controller;
 
 /**
  * Class PermissionController.
  */
-class PermissionController extends AbstractApiController
+class PermissionController extends Controller
 {
-    /**
-     * @return mixed
-     */
-    public function index()
-    {
-        $query = Permission::query()->with('groups');
-        $lists = $query->get()->groupBy(function ($item) {
-            list($client, $module, $function, $action) = explode('.', $item['name']);
 
-            return $action;
-        });
-        $results = [];
-        $actionValues = ['create' => '创建', 'show' => '查看', 'update' => '编辑', 'delete' => '删除'];
-        foreach ($lists as $key => $list) {
-            foreach ($list as $item) {
-                if (array_key_exists($key, $actionValues)) {
-                    $key = $actionValues[$key];
-                }
-                $results[$key][] = [
-                    'id'           => $item->id,
-                    'name'         => $item->name,
-                    'display_name' => $item->display_name,
-                    'description'  => $item->description,
-                    'group'        => $item->groups->implode('display_name', '|'),
-                ];
-            }
-        }
-
-        return $this->respondWithArray($results);
-    }
-
-    /**
-     * @param $perm_id
-     *
-     * @return array|mixed
-     */
-    public function show($perm_id)
-    {
-        $perm = Permission::query()->find(intval($perm_id));
-        if (!$perm || !$perm->exists) {
-            return $this->errorNotFound();
-        }
-
-        return $this->respondWithItem($perm, function (Permission $list) {
-            return [
-                'id'           => $list->id,
-                'name'         => $list->name,
-                'display_name' => $list->display_name,
-                'description'  => $list->description,
-                'group'        => $list->groups->implode('display_name', '|'),
-            ];
-        });
-    }
-
-    /**
-     * 权限添加和编辑方法
-     *
-     * @return mixed
-     */
-    public function store()
-    {
-        $validator = $this->getValidationFactory()->make(
-            $this->request->all(),
-            [
-                'name'         => 'required',
-                'display_name' => 'required',
-            ],
-            [
-                'name.required'         => '请输入名称.',
-                'display_name.required' => '请输入显示名称.',
-            ]
-        );
-        if ($validator->fails()) {
-            return $this->errorValidate($validator->getMessageBag()->toArray());
-        }
-        Permission::addPermission(
-            $this->request->name,
-            $this->request->display_name,
-            $this->request->input('description', null)
-        );
-
-        return $this->respondWithSuccess('成功!');
-    }
-
-    /**
-     * @param $id
-     *
-     * @return array|mixed
-     */
-    public function destroy($id)
-    {
-        $perm = Permission::query()->find($id);
-        if (! $perm || ! $perm->exists) {
-            return $this->errorNotFound();
-        }
-
-        if (! $perm->delete()) {
-            return $this->errorInternal();
-        }
-
-        return $this->respondWithSuccess('删除成功!');
-    }
 }
