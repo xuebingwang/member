c1efe30ad696f25f85ff27da3359e1c3deb8c1cf
twilroad <269044570@qq.com>
2017-05-04 16:17:34 +0800

    add logic for tag/create api

--- src/Handlers/Tag/CreateHandler.php
+++ src/Handlers/Tag/CreateHandler.php
@@ -8,11 +8,48 @@
  */
 namespace Notadd\Member\Handlers\Tag;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Notadd\Foundation\Passport\Abstracts\SetHandler;
+use Notadd\Member\Models\MemberTag;
 
 /**
  * Class CreateHandler.
  */
 class CreateHandler extends SetHandler
 {
+    /**
+     * Execute Handler.
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public function execute()
+    {
+        $this->validate($this->request, [
+            'tag' => 'required',
+        ], [
+            'tag.required' => $this->translator->trans('必须填写标签名称！'),
+        ]);
+        $created = new Collection();
+        if (Str::contains(',', $this->request->input('tag'))) {
+            $tags = explode(',', $this->request->input('tag'));
+            foreach ($tags as $tag) {
+                $created->push(MemberTag::query()->create([
+                    'tag' => $tag,
+                ]));
+            }
+        } else {
+            $created->push(MemberTag::query()->create([
+                'tag' => $this->request->input('tag'),
+            ]));
+        }
+        if ($list = $this->request->input('list')) {
+            $list = explode(PHP_EOL, $list);
+        }
+
+        $this->messages->push($this->translator->trans('创建用户标签成功！'));
+
+        return true;
+    }
 }
