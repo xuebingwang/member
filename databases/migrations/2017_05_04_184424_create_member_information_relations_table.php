9c5b8621918a7272f256ff9d7c84b34ede9c3c9d
twilroad <269044570@qq.com>
2017-05-04 18:45:59 +0800

    create migration file for member_information_relations table

---
+++ databases/migrations/2017_05_04_184424_create_member_information_relations_table.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @datetime 2017-05-04 18:44:24
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Notadd\Foundation\Database\Migrations\Migration;
+
+/**
+ * Class CreateMemberInformationRelationsTable.
+ */
+class CreateMemberInformationRelationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->create('member_information_relations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->drop('member_information_relations');
+    }
+}
