0044226e5f2cc4ea5a7b98ebd66de54fbc46d44b
twilroad <269044570@qq.com>
2017-05-05 20:56:23 +0800

    add logic for permission/get api

--- src/Handlers/Permission/GetHandler.php
+++ src/Handlers/Permission/GetHandler.php
@@ -8,11 +8,45 @@
  */
 namespace Notadd\Member\Handlers\Permission;
 
+use Illuminate\Container\Container;
+use Illuminate\Support\Collection;
 use Notadd\Foundation\Passport\Abstracts\DataHandler;
+use Notadd\Foundation\Permission\PermissionManager;
+use Notadd\Member\Models\MemberGroup;
 
 /**
  * Class GetHandler.
  */
 class GetHandler extends DataHandler
 {
+    /**
+     * @var \Notadd\Foundation\Permission\PermissionManager
+     */
+    protected $permission;
+
+    /**
+     * GetHandler constructor.
+     *
+     * @param \Illuminate\Container\Container                 $container
+     * @param \Notadd\Foundation\Permission\PermissionManager $permission
+     */
+    public function __construct(Container $container, PermissionManager $permission)
+    {
+        parent::__construct($container);
+        $this->permission = $permission;
+    }
+
+    /**
+     * Data for handler.
+     *
+     * @return array
+     */
+    public function data()
+    {
+        $data = new Collection();
+        $data->put('groups', MemberGroup::all());
+        $data->put('permissions', $this->permission->groups());
+
+        return $data->toArray();
+    }
 }
