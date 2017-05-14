174af1d457624b6e43c0e74b3f470eca4505ad17
twilroad <269044570@qq.com>
2017-05-14 16:56:09 +0800

    add logic of common notification

--- src/Handlers/Notification/SendHandler.php
+++ src/Handlers/Notification/SendHandler.php
@@ -9,6 +9,8 @@
 namespace Notadd\Member\Handlers\Notification;
 
 use Notadd\Foundation\Passport\Abstracts\SetHandler;
+use Notadd\Member\Models\Member;
+use Notadd\Member\Notifications\CommonNotify;
 
 /**
  * Class SendHandler.
@@ -17,6 +19,38 @@ class SendHandler extends SetHandler
 {
     public function execute()
     {
+        $this->validate($this->request, [
+            'content' => 'required',
+            'title' => 'required',
+            'users' => 'required',
+            'way' => 'required',
+        ], [
+            'content.required' => '必须填写消息内容！',
+            'title.required' => '必须填写消息标题！',
+            'users.required' => '必须选择用户！',
+            'way.required' => '必须选择发送方式！',
+        ]);
+        $users = collect($this->request->input('users'));
+        if ($users->isEmpty()) {
+            $this->code = 500;
+            $this->errors->push($this->translator->trans('发送用户列表为空！'));
+
+            return false;
+        }
+        $users->transform(function ($id) {
+            if (Member::query()->where('id', $id)->count()) {
+                return Member::query()->find($id);
+            } else {
+                return Member::query()->find(1);
+            }
+        });
+        $users->each(function (Member $user) {
+            $user->notify(new CommonNotify([
+                'content' => $this->request->input('content'),
+                'title' => $this->request->input('title'),
+            ]));
+        });
+
         return true;
     }
 }
