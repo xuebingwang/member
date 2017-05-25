2473422692896e76c555c4c45112f66865aebaee
twilroad <269044570@qq.com>
2017-05-25 18:28:42 +0800

    add base structure for permission page

--- resources/mixes/administration/src/pages/GroupPermission.vue
+++ resources/mixes/administration/src/pages/GroupPermission.vue
@@ -10,6 +10,7 @@
                 next(vm => {
                     vm.groups = data.groups;
                     vm.permissions = data.permissions;
+                    vm.types = data.types;
                     injection.loading.finish();
                     injection.sidebar.active('member');
                 });
@@ -21,10 +22,37 @@
             return {
                 groups: [],
                 permissions: [],
+                types: [],
             };
         },
     };
 </script>
 <template>
-
+    <div class="member-warp">
+        <div class="group-permission">
+            <card :bordered="false">
+                <p slot="title">用户组权限</p>
+                <ul class="groups">
+                    <li v-for="type in types">{{ type.name }}</li>
+                </ul>
+                <collapse value="global">
+                    <panel :name="permission.attributes.identification" v-for="permission in permissions">
+                        {{ permission.attributes.name }}
+                        <template slot="content">
+                            <div class="list" v-for="p in permission.permissions">
+                                <ul>
+                                    <li>{{ p.description }}</li>
+                                    <li v-for="t in types">
+                                        <i-select multiple>
+                                            <i-option :key="g" :value="g.identification" v-for="g in groups">{{ g.name }}</i-option>
+                                        </i-select>
+                                    </li>
+                                </ul>
+                            </div>
+                        </template>
+                    </panel>
+                </collapse>
+            </card>
+        </div>
+    </div>
 </template>
