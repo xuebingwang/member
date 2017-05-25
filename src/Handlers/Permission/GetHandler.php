2cb24191ef41bf3e30510ed627da7ebc9449f603
twilroad <269044570@qq.com>
2017-05-25 16:55:37 +0800

    format permission data to array

--- src/Handlers/Permission/GetHandler.php
+++ src/Handlers/Permission/GetHandler.php
@@ -2,15 +2,17 @@
 /**
  * This file is part of Notadd.
  *
- * @author TwilRoad <269044570@qq.com>
+ * @author        TwilRoad <269044570@qq.com>
  * @copyright (c) 2017, notadd.com
- * @datetime 2017-05-05 20:48
+ * @datetime      2017-05-05 20:48
  */
 namespace Notadd\Member\Handlers\Permission;
 
 use Illuminate\Container\Container;
 use Illuminate\Support\Collection;
 use Notadd\Foundation\Passport\Abstracts\DataHandler;
+use Notadd\Foundation\Permission\Permission;
+use Notadd\Foundation\Permission\PermissionGroup;
 use Notadd\Foundation\Permission\PermissionManager;
 use Notadd\Foundation\Permission\PermissionTypeManager;
 use Notadd\Member\Models\MemberGroup;
@@ -51,9 +53,21 @@ class GetHandler extends DataHandler
      */
     public function data()
     {
+        $permissions = $this->permission->groups();
+        $permissions->transform(function (PermissionGroup $group) {
+            $attributes = $group->attributes();
+            $permissions = $group->permissions()->transform(function (Permission $permission) {
+                return $permission->attributes();
+            })->toArray();
+
+            return [
+                'attributes'  => $attributes,
+                'permissions' => $permissions,
+            ];
+        });
         $data = new Collection();
         $data->put('groups', MemberGroup::all());
-        $data->put('permissions', $this->permission->groups());
+        $data->put('permissions', $permissions->toArray());
         $data->put('types', $this->type->types());
 
         return $data->toArray();
