2abed43d6536dc8448d141ad2bf4356680c1b276
twilroad <269044570@qq.com>
2017-05-26 11:18:13 +0800

    fix summaries

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -25,12 +25,12 @@ use Notadd\Foundation\Routing\Abstracts\RouteRegister as AbstractRouteRegister;
 use Notadd\Member\Controllers\Api\UploadController;
 
 /**
- * Class RouteRegistrar.
+ * Class RouteRegister.
  */
 class RouteRegister extends AbstractRouteRegister
 {
     /**
-     * Handle Route Registrar.
+     * Handle Route Register.
      */
     public function handle()
     {
