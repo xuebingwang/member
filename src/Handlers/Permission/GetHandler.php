53ad0f3a259f4e688c391dcb987ee0ea3d70334f
twilroad <269044570@qq.com>
2017-05-27 12:11:49 +0800

    refactor logic for permissions getter

--- src/Handlers/Permission/GetHandler.php
+++ src/Handlers/Permission/GetHandler.php
@@ -13,7 +13,10 @@ use Illuminate\Support\Collection;
 use Notadd\Foundation\Passport\Abstracts\Handler;
 use Notadd\Foundation\Permission\Permission;
 use Notadd\Foundation\Permission\PermissionGroup;
+use Notadd\Foundation\Permission\PermissionGroupManager;
 use Notadd\Foundation\Permission\PermissionManager;
+use Notadd\Foundation\Permission\PermissionModule;
+use Notadd\Foundation\Permission\PermissionModuleManager;
 use Notadd\Foundation\Permission\PermissionType;
 use Notadd\Foundation\Permission\PermissionTypeManager;
 use Notadd\Member\Models\MemberGroup;
@@ -23,6 +26,16 @@ use Notadd\Member\Models\MemberGroup;
  */
 class GetHandler extends Handler
 {
+    /**
+     * @var \Notadd\Foundation\Permission\PermissionGroupManager
+     */
+    protected $group;
+
+    /**
+     * @var \Notadd\Foundation\Permission\PermissionModuleManager
+     */
+    protected $module;
+
     /**
      * @var \Notadd\Foundation\Permission\PermissionManager
      */
@@ -36,13 +49,17 @@ class GetHandler extends Handler
     /**
      * GetHandler constructor.
      *
-     * @param \Illuminate\Container\Container                     $container
-     * @param \Notadd\Foundation\Permission\PermissionManager     $permission
-     * @param \Notadd\Foundation\Permission\PermissionTypeManager $type
+     * @param \Illuminate\Container\Container                       $container
+     * @param \Notadd\Foundation\Permission\PermissionGroupManager  $group
+     * @param \Notadd\Foundation\Permission\PermissionModuleManager $module
+     * @param \Notadd\Foundation\Permission\PermissionManager       $permission
+     * @param \Notadd\Foundation\Permission\PermissionTypeManager   $type
      */
-    public function __construct(Container $container, PermissionManager $permission, PermissionTypeManager $type)
+    public function __construct(Container $container, PermissionGroupManager $group, PermissionModuleManager $module, PermissionManager $permission, PermissionTypeManager $type)
     {
         parent::__construct($container);
+        $this->group = $group;
+        $this->module = $module;
         $this->permission = $permission;
         $this->type = $type;
     }
@@ -54,24 +71,31 @@ class GetHandler extends Handler
      */
     protected function execute()
     {
-
         $data = new Collection();
         $data->put('groups', MemberGroup::all());
-        $permissions = new Collection();
-        $this->permission->groups()->each(function (PermissionGroup $group) use ($permissions) {
-            $attributes = $group->attributes();
-            $list = new Collection();
-            $group->permissions()->each(function (Permission $permission) use ($list) {
-                $list->push($permission->attributes());
+        $modules = new Collection();
+        $this->module->list()->each(function (PermissionModule $module) use ($modules) {
+            $attributes = $module->attributes();
+            $groups = new Collection();
+            $this->group->groupsForModule($module->identification())->each(function (PermissionGroup $group) use ($groups, $module) {
+                $attributes = $group->attributes();
+                $permissions = new Collection();
+                $this->permission->permissionsForGroup($module->identification() . '::' . $group->identification())->each(function (Permission $permission) use ($permissions) {
+                    $permissions->push($permission->attributes());
+                });
+                $groups->push([
+                    'attributes'  => $attributes,
+                    'permissions' => $permissions,
+                ]);
             });
-            $permissions->push([
-                'attributes'  => $attributes,
-                'permissions' => $list,
+            $modules->push([
+                'attributes' => $attributes,
+                'groups'     => $groups,
             ]);
         });
-        $data->put('permissions', $permissions->toArray());
+        $data->put('modules', $modules->toArray());
         $types = new Collection();
-        $this->type->types()->transform(function (PermissionType $type) use ($types) {
+        $this->type->types()->each(function (PermissionType $type) use ($types) {
             $types->push([
                 'attributes' => $type->attributes(),
                 'has'        => $type->has(),
